c67562fcece5ac16f6d90cbbc9016db59ffa6750
edavelli.poojareddy <edavelli.poojareddy@LP-PF2XF41Y.HCLT.CORP.HCL.IN>
2021-08-27 17:09:07 +0530

    BDD is commited

--- BDD/src/test/java/Runner/Run1.java
+++
@@ -1,9 +0,0 @@
-package Runner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="Features", glue="stepDefinition",tags="@addToCart")
-public class Run1 extends AbstractTestNGCucumberTests {
-
-}
